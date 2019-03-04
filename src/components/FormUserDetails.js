fcc08756c572a033313a6cdd1fe0be718c3684a9
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-03-04 19:34:02 +0530

    second commit

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -36,10 +36,21 @@ export class FormUserDetails extends Component {
                 onChange={handleChange('email')}
                 defaultValue={values.email}
             />
+            <br />
+            <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+            />
         </React.Fragment>
       </MuiThemeProvider>
     )
   }
 }
-
+const styles = {
+  button:{
+    margin : 15
+  }
+}
 export default FormUserDetails
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import FormUserDetails from './FormUserDetails';
-
+import FormPersonalDetails from './FormPersonalDetails';
 
 
 export class UserForm extends Component {
@@ -58,7 +58,14 @@ prevStep = () => {
                 />
             )
         case 2:
-            return <h1>FormPersonalDetails</h1>
+        return (
+            <FormPersonalDetails 
+                nextStep={this.nextStep}
+                prevStep={this.prevStep}
+                handleChange={this.handleChange}
+                values={values}
+            />
+        )
         case 3:
             return <h1>Confirm</h1>
         case 4:
